cd3638f661f95ddea097319c6d20555d8eb0edec
Igor Serpak <igor.serpak@gmail.com>
2014-11-09 21:30:47 +0200

    Tinkered with mongodb

--- helpers.rb
+++ helpers.rb
@@ -1,19 +1,33 @@
 require 'sinatra/base'
 
 module Sinatra
-  
-  module DataHelpers
-    def make_row(cells)
-      "<tr>" << cells.map { |cell| "<td>#{cell}</td>" }.join << "</tr>"
+
+  module MongoHelpers
+    
+    # a helper method to turn a string ID
+    # representation into a BSON::ObjectId
+    def object_id val
+      BSON::ObjectId.from_string(val)
     end
     
+    def document_by_id id
+      id = object_id(id) if String === id
+      settings.mongo_db['memory'].find_one(:_id => id).to_json
+    end
+
+  end
+ 
+  module DataHelpers
+    
     def make_table(rows)
+      def make_row(cells)
+        "<tr>" << cells.map { |cell| "<td>#{cell}</td>" }.join << "</tr>"
+      end
       "<table>" << rows.map { |row| make_row(row) }.join << "</table>"
     end
     
-    def stats_to_table(stats)
-      stats = stats.split("\n").map { |row| row.split(%r{\s+}) }
-      make_table(stats)
+    def disk
+      `df -h`.sub!('Mounted on', 'Mounted_on').split("\n").map { |row| row.split(%r{\s+}) }
     end
     
     def get_memory
@@ -29,8 +43,79 @@ module Sinatra
       end.join
       table << "</tr></table>"
     end
+
+    def memory
+      data = `free -m | grep 'Mem'`.scan(/\w+/).drop(1)
+      names = ['total', 'used', 'free', 'shared', 'buffers', 'cached']
+      memory = {}
+      0.upto(data.length - 1) do |i|
+        memory[names[i]] = data[i]
+      end
+      memory["time"] = Time.now
+      memory
+    end
+
+    def memory_history
+
+      def make_row row_data
+        row = "<tr>"
+        row_data.values.drop(1).map do |td|
+          row << "<td>#{td}</td>"
+        end
+        row << "</tr>"
+      end
+
+      def make_header_row
+        header_row = "<tr>"
+        names = ['total', 'used', 'free', 'shared', 'buffers', 'cached', 'time']
+        header_row << names.map { |n| "<td>#{n.capitalize}</td>" }.join
+        header_row << "</tr>"
+      end
+
+      mem_history = settings.mongo_db['memory'].find.to_a
+
+      table = "<table>"
+      table << make_header_row
+      
+      mem_history.each do |row_data|
+        table << make_row(row_data)
+      end
+      
+      table << "</table>"
+    end
+    
+    def save_memory_data
+      settings.mongo_db['memory'].insert memory
+    end
+
+    def save_disk_data
+      disk_data = disk
+      document = {"array" => Array.new, "time" => Time.now}
+      def make_doc(row)
+        doc = {}
+        names = ['filesystem', 'size', 'used', 'avail', 'use%', 'mounted_on']
+        row.each_with_index do |value, index|
+          doc[names[index]] = row[index]
+        end
+        doc
+      end
+      disk_data.each_with_index do |row, index|
+        document["array"].push(make_doc(row)) unless index == 0
+      end
+      settings.mongo_db['disk'].insert document
+    end
+
+    def disk_history
+      data = settings.mongo_db['disk'].find.to_a
+      table = "<table><tr><td>Time</td><td>Size</td><td>Used</td></tr>"
+      data.each do |r|
+        table << "<tr><td>#{r["time"]}</td><td>#{r["array"][0]["size"]}</td><td>#{r["array"][0]["used"]}</td></td>"
+      end
+      table << "</table>"
+    end
+    
   end
   
   helpers DataHelpers
-  
+  helpers MongoHelpers
 end
--- index.rb
+++ index.rb
@@ -1,14 +1,42 @@
+require 'rubygems'
 require 'sinatra'
+require 'mongo'
 require './helpers'
+
+include Mongo
+
+configure do
+  conn = MongoClient.new("localhost", 27017)
+  set :mongo_connection, conn
+  set :mongo_db, conn.db('test')
+end
 
 get '/' do
   erb :index
 end
 
-get '/disk' do
+get '/disk/?' do
   erb :disk
 end
 
-get '/memory' do
+get '/memory/?' do
   erb :memory
+end
+
+get '/memory/history/?' do
+  erb :memory_history
+end
+
+get '/disk/history/?' do
+  erb :disk_history
+end
+
+post '/save_memory_data' do
+  save_memory_data
+  redirect to('/memory')
+end
+
+post '/save_disk_data' do
+  save_disk_data
+  redirect to('/disk')
 end
